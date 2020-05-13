977e366dda20ba43e600b37e0cf89c4fc75171c1
GuillaumePubert <guillaume.pubert@isen-ouest.yncrea.fr>
2020-05-13 18:25:58 +0200

    EX 3

---
+++ Ex 3.py
@@ -0,0 +1,28 @@
+from PySide2.QtWidgets import *
+from PySide2 import QtGui
+from PySide2 import QtCore
+class Window(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.setFixedSize(400,300)
+        self.setWindowTitle("IHM")
+        self.icon=QtGui.QIcon('C:/Users/guill/Documents/Pythonfr-flag.png')
+        self.setWindowIcon(self.icon)
+        self.layoutV=QVBoxLayout()
+        self.label=QLabel("Hello world")
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.barre=QProgressBar()
+        self.barre.setValue(50)
+        self.ligne=QLineEdit("")
+        self.bouton=QPushButton("Click me !")
+        self.layoutV.addWidget(self.label)
+        self.layoutV.addWidget(self.barre)
+        self.layoutV.addWidget(self.ligne)
+        self.layoutV.addWidget(self.bouton)
+        self.setLayout(self.layoutV)
+if __name__ == "__main__":
+   app = QApplication([])
+   win = Window()
+   win.show()
+   win.show()
+   app.exec_()
